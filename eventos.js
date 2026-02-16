b9f39e294e16da75b34e3d6ecc440209b7e7c36a
torodecuerdacarcabuey <torocarcabueysocial@gmail.com>
2026-02-16 23:20:11 +0100

    Create eventos.js

---
+++ eventos.js
@@ -0,0 +1,21 @@
+// Edita SOLO esta lista para cambiar los eventos
+const EVENTOS = [
+  {
+    dia: "28",
+    mes: "FEB",
+    titulo: "Gala Anual de la Federación Española de Toro con Cuerda",
+    descripcion: "Acto informativo y novedades de la asociación.",
+  },
+  {
+    dia: "12",
+    mes: "ABR",
+    titulo: "Visita a la ganaderia Partido de Resina",
+    descripcion: "Este año la conoceremos la prestigiosa ganaderia de Partido de Resina",
+  },
+  {
+    dia: "2-5",
+    mes: "JUL",
+    titulo: "XXI Congreso Nacional de Toro con Cuerda.",
+    descripcion: "Amposta.",
+  },
+];
