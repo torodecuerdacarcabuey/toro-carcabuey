c8d56ab79add407b81ec18cc4d9db20e17605404
torodecuerdacarcabuey <torocarcabueysocial@gmail.com>
2026-02-22 21:03:38 +0100

    Create noticias.js with sample news data
    Added a new noticias.js file with sample news articles.

---
+++ noticias.js
@@ -0,0 +1,27 @@
+// noticias.js
+// Para añadir una noticia: copia un bloque y pégalo ARRIBA del todo.
+// Para cambiar imagen: cambia "imagen" por "assets/noticias/tuimagen.jpg"
+
+const NOTICIAS = [
+  {
+    fecha: "2026-02-22",
+    titulo: "Escribe aquí el título de la noticia",
+    resumen: "Resumen corto (1–2 líneas). Si quieres poner más, se puede.",
+    imagen: "assets/noticias/n1.jpg",
+    enlace: "" // opcional: "historia.html" o "https://..."
+  },
+  {
+    fecha: "2026-02-10",
+    titulo: "Otra noticia de ejemplo",
+    resumen: "Texto breve para que sea fácil de leer.",
+    imagen: "assets/noticias/n2.jpg",
+    enlace: ""
+  }
+  {
+    fecha: "2026-02-10",
+    titulo: "Otra noticia de ejemplo",
+    resumen: "Texto breve para que sea fácil de leer.",
+    imagen: "assets/noticias/n2.jpg",
+    enlace: "https://www.diariocordoba.com/cultura/2025/08/23/carcabuey-vive-intensa-jornada-toro-120878774.html"
+  }
+];
